9660b86828157dd5ccd65a84de282252f1dcdf9d
KrzysztofHa <112620293+KrzysztofHa@users.noreply.github.com>
2024-01-17 23:54:22 +0100

    Add project files.

---
+++ DotNetEx/Data.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static DotNetEx.CheckData;
+namespace DotNetEx
+{
+    internal class Data
+    {
+        public string  Name { get; }
+        public string Secname { get; }
+        public char Sex { get; }
+        public ulong Tel { get; }
+        public ulong Pesel { get; } //11 liczb
+        public string Email { get; }
+       
+
+        
+        public Data()
+        {
+            Console.WriteLine("Program for loading data from the user");
+
+            Name = string.Empty;
+            while (string.IsNullOrEmpty(Name))
+            {
+                Console.Clear();
+                Console.WriteLine("Enter name");
+                Name = Console.ReadLine();
+            }
+
+            Secname = string.Empty;
+            while (string.IsNullOrEmpty(Secname))
+            {
+
+                Console.Clear();
+                Console.WriteLine("Enter secend name ");
+                Secname = Console.ReadLine();
+
+            }
+
+            
+            ConsoleKeyInfo ex;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter  sex \n woman pres key  'W' or men press key 'M'");
+                ex = Console.ReadKey(true);
+            } while(ex.Key != ConsoleKey.W & ex.Key != ConsoleKey.M);
+            Sex = ex.KeyChar;
+
+            string data = string.Empty;
+            bool check = false;
+            ulong d = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter your mobile phone number with 9 digits");
+                data = Console.ReadLine();
+                check = ulong.TryParse(data, out d);
+
+                if (check)
+                {
+                    if (data.Length == 9)
+                    {
+                        Tel = d; break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Enter a valid positive number containing a maximum of 9 digits");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You have not entered any number or the number is incorrect");
+                    Console.WriteLine("Enter a valid positive number containing a maximum of 9 digits");                    
+                }
+
+            } while (true);
+
+            data = string.Empty;
+            check = false;
+            d = 0;
+            do
+            {               
+                Console.Clear();
+                Console.WriteLine("Enter your PESEL number maximum of 11 digits");
+                data = Console.ReadLine();
+                check = ulong.TryParse(data, out d);
+
+                if (check)
+                {
+                    if (data.Length == 11)
+                    {
+                        Pesel = d; break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Enter a valid positive number containing a maximum of 11 digits");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You have not entered any number or the number is incorrect");
+                    Console.WriteLine("Enter a valid positive number containing a maximum of 11 digits");                    
+                }
+
+            } while (true);
+
+
+
+            Console.Clear();
+            //CheckData checkdata = new CheckData();
+            do
+            {
+                Console.WriteLine("\n Enter your email address");
+                Console.WriteLine("\n letters, numbers, underscore (_ ), dot (.), dash (-) are allowed. \nThe identifier cannot begin with an underscore" +
+                    " (_ ), dot (.) or dash (-), e.g.: _kowlaski@firma. com.pl. \nthe identifier cannot contain spaces, e.g.: jan kowlaski@firma.com.pl.");
+                data = Console.ReadLine();                
+                if (IsValidEmail(data))
+                {
+                    Email = data;
+                    break;
+                }
+            } while (true);            
+        }
+        
+        public void DataRead()
+        {
+            Console.WriteLine("Profil\n name {0}\n secendname {1}\n sex {2}\n nember telephon {3}\n nr. pesel {4}\n " +
+                "email {5}\n ",
+                Name, Secname, Sex, Tel, Pesel, Email);
+            Console.WriteLine("Press any key to EXIT");
+            Console.ReadKey();
+        }       
+    }
+}
---
+++ DotNetEx/Program.cs
@@ -0,0 +1,125 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace DotNetEx
+
+{
+    class Program
+    {
+       
+
+        private static void Main(string[] args)
+        {
+            //Lekcja 2/4 
+            //zadanie 1  program deklarujący zmienne danych pracownika firmy
+            
+            string name;
+            string sec_name;
+            decimal age; //może ktoś podać na przykład 43,5
+            char sex;
+            ulong pesel;
+            ulong nr_worker;
+
+            Console.WriteLine("A program that declares variables in code");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+
+            //zadanie 2 zadeklarować zmienne i wypisać je w odwrotnej kolejności niż zadeklarowano
+
+            string first = "w";
+            string sec = "z";
+            string the = "x";
+
+            Console.WriteLine("The program declares three variables in the code and displays them in reverse order");
+            Console.WriteLine(the);
+            Console.WriteLine(sec);
+            Console.WriteLine(first);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            // Zadanie 3 program do obliczania długości przeciwprostokątnej
+
+            double[] a = new double[2] { 0, 0 };
+            string t = "";
+            bool calculate = false;
+            
+            Console.Clear();
+            Console.WriteLine("Calculation of hypotenuse unit mm");
+            Console.WriteLine("Enter the numbers 'a' and 'b' of the sides of the rectangle:");
+            Console.WriteLine("positive and range from 1 - 20");
+
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+
+                do
+                {                 
+                    if (a[a.Length - 2] == 0)
+                    {
+                        Console.WriteLine("Enter the number 'a'");
+                        t = Console.ReadLine();
+                    }
+                    else if (a[a.Length - 1] == 0)
+                    {
+                        Console.WriteLine("Enter the number 'b'");
+                        t = Console.ReadLine();
+                    }
+                    if (Double.TryParse(t, out a[i]) & (a[i] > 0) & (a[i] < 21))
+                    {
+
+                        Console.WriteLine("The number is correct \n");
+                        a[i] = a[i];
+                        if (a.Length - 1 == i)
+                        {
+                            calculate = true;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        a[i] = 0;
+                        Console.Clear();
+                        Console.WriteLine("You have not entered any number or the number is incorrect");
+                        Console.WriteLine("Enter a valid positive number between 1-20");
+                    }
+                }
+                while (true);
+                {
+                    if (calculate)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("number 'a': {0}mm number 'b': {1}mm \n", a[0], a[1]);
+                        double j = (Math.Pow(a[0], 2) + Math.Pow(a[1], 2));
+                        Console.WriteLine("length of the hypotenuse = {0}mm\n", j);
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    }
+                }
+
+               
+            }
+
+            // Zadanie 4 stwórz dwie zmienne liczbowe i jedną tekstową i przypisz im wartości:
+            // a. 10
+            // b. Szkoła Dotneta
+            // c. 12.5
+
+            int aa;
+            string bb;
+            double cc;
+
+            aa = 10;
+            bb = "Szkoła Dotneta";
+            cc = 12.5;
+        
+            // Zadanie 5 Napisz program w którym użytkownik poda jak najwięcej danych o sobie
+            var data = new Data();
+            data.DataRead();
+             
+
+        }
+    }
+}
